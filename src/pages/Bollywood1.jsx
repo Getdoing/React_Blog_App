8747c12bf458457c7551cf25048cdd4c658a1e78
nagesh <nagesh7460@gmail.com>
2022-10-17 11:50:59 +0530

    hello

---
+++ src/pages/Bollywood1.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card1 from '../Components/Cards/Card1'
+import LatestArticle from './LatestArticles/LatestArticle';
+import SideBar from './SideBar/SideBar';
+// import { Fitness } from '../HomeData';
+const Bollywood = () => {
+    // const [fitness, setFitness] = useState(Fitness)
+    return (
+        <>
+            <h1 className='titleName'>🎥BOLLYWOOD🎥</h1>
+            <section className='container'>
+                {/* <Card1 /> */}
+            </section>
+            <section className='container LatestSide'>
+                <LatestArticle title={'LATEST - BOLLYWOOD NEWS'} />
+                <SideBar title={'TOP-BOLLYWOOD POSTS'} />
+            </section>
+            <section className='container'>
+                <Card1 />
+            </section>
+        </>
+    )
+}
+export default Bollywood;
---
+++ src/pages/Food1.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card1 from '../Components/Cards/Card1'
+import LatestArticle from './LatestArticles/LatestArticle';
+import SideBar from './SideBar/SideBar';
+// import { Fitness } from '../HomeData';
+const Food = () => {
+    // const [fitness, setFitness] = useState(Fitness)
+    return (
+        <>
+            <h1 className='titleName'>🥗FOOD🥗</h1>
+            <section className='container'>
+                {/* <Card1 /> */}
+            </section>
+            <section className='container LatestSide'>
+                <LatestArticle title={'LATEST - FOOD NEWS'} />
+                <SideBar title={'TOP-FOOD POSTS'} />
+            </section>
+            <section className='container'>
+                <Card1 />
+            </section>
+        </>
+    )
+}
+export default Food;
